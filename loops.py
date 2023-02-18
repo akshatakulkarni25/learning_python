1eccffb1161a58a9fdd243b4c098ebc96c6044b5
akshatakulkarni <akshatakulkarni2509@gmail.com>
2023-02-18 23:47:54 +0530

    Added how for loop works

--- loops.py
+++ loops.py
@@ -1,8 +1,54 @@
 # Loops in Python
-# for loop
+# for loop : Used for sequential traversal
+# for i in range(0,11):
+#     print(i)
+
+
 # while loop
+# while is used to execute a block of stmts repeatedly until a condition is satisfied
+# python uses indentation as a method to group statements for a block
+
+# count = 0
+# while (count < 3):
+#     count = count + 1
+#     print("Hello Geek")
+
+# Else can also be used with while and it is executed when the while condition is false. 
+#the else block is not executed if we break out of while loop or if an exception is raised
+# while(condition):
+#   stmts
+# else:
+#   stmts
 
 
+# Loop control statements
+# 1. Continue: brings control to the beginning of the loop
+# for i in range(0,11):            prints only odd numbers
+#   if(i%2 == 0):
+#       continue
+#   else:
+#       print(i)
+
+# 2. break : brings control out of the loop
+# for i in range(2,11):            prints all prime numbers            
+#   for j in range(2,i):
+#       if(i%j==0):
+#           break
+#    else:
+#        print(i)
+
+# 3. pass : it is used to write empty loops
+#   for i in range(0,101):
+#       pass
+
+# For loop behind the scene uses an iterator and loops the iterable objet using next() function inside a while True block
+# num_iterator = iter(numbers)
+# while True:
+#     try:
+#         # get the next item
+#         number = next(num_iterator)
+#     except StopIteration:
+#         break
 
 fruits= ['Apple','Banana','Chikoo','Dragonfruit']
 for fruit in fruits:
@@ -31,3 +77,10 @@ for number in range(1,101):
         print("Buzz")
     else:
         print(number)
+
+for i in range(2,11):            
+    for j in range(2,i):
+        if(i%j==0):
+            break
+    else:
+        print(i)
